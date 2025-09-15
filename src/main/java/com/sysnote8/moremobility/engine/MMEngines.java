d3c906850d7b91293b397f28fe050e9e10eb702c
sysnote8main <sysnote8main@gmail.com>
2025-09-15 20:28:41 +0900

    fix: exhaust position of netherite engine

--- src/main/java/com/sysnote8/moremobility/engine/MMEngines.java
+++ src/main/java/com/sysnote8/moremobility/engine/MMEngines.java
@@ -16,7 +16,7 @@ public class MMEngines {
                             Automobility.rl("textures/entity/automobile/engine/netherite.png"), MoreMobility.id("engine_netherite"),
                             new AutomobileEngine.ExhaustPos(3, 3.8f, -7.6f, 40, 0),
                             new AutomobileEngine.ExhaustPos(-3, 3.8f, -7.6f, 40, 0),
-                            new AutomobileEngine.ExhaustPos(-1.5f, 1.0f, -2.8f, 40, 0),
+                            new AutomobileEngine.ExhaustPos(0.0f, 6.5f, -5.35f, 40, 0),
                             new AutomobileEngine.ExhaustPos(4, 7.075f, -4.95f, 40, 0),
                             new AutomobileEngine.ExhaustPos(-4, 7.075f, -4.95f, 40, 0)
                     )
