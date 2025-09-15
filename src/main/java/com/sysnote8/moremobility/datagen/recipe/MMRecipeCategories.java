66b8fefa01919d679936a1b47ed85d274b833ba0
sysnote8main <sysnote8main@gmail.com>
2025-09-15 18:01:25 +0900

    fix: recipe category for auto mechanic table

--- src/main/java/com/sysnote8/moremobility/datagen/recipe/MMRecipeCategories.java
+++ src/main/java/com/sysnote8/moremobility/datagen/recipe/MMRecipeCategories.java
@@ -1,8 +1,11 @@
 package com.sysnote8.moremobility.datagen.recipe;
 
-import com.sysnote8.moremobility.MoreMobility;
+import io.github.foundationgames.automobility.Automobility;
 import net.minecraft.resources.ResourceLocation;
 
 public class MMRecipeCategories {
-    public static final ResourceLocation ENGINES = MoreMobility.id("engines");
+    public static final ResourceLocation ATTACHMENTS = Automobility.rl("attachments");
+    public static final ResourceLocation ENGINES = Automobility.rl("engines");
+    public static final ResourceLocation FRAMES = Automobility.rl("frames");
+    public static final ResourceLocation WHEELS = Automobility.rl("wheels");
 }
