87e86313ce48a36c69dd8bed470cebbf6104a386
sysnote8main <sysnote8main@gmail.com>
2025-10-22 18:36:08 +0900

    feat: register all auto mechanic table recipes

--- src/main/java/com/sysnote8/moremobility/integration/jei/AutoMechanicTableJeiCategory.java
+++ src/main/java/com/sysnote8/moremobility/integration/jei/AutoMechanicTableJeiCategory.java
@@ -1,28 +1,25 @@
 package com.sysnote8.moremobility.integration.jei;
 
-import com.sysnote8.moremobility.MoreMobility;
 import com.sysnote8.moremobility.integration.SharedTexture;
 import io.github.foundationgames.automobility.recipe.AutoMechanicTableRecipe;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.recipe.IFocusGroup;
-import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.AbstractRecipeCategory;
 import mezz.jei.common.gui.elements.DrawableResource;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.crafting.Ingredient;
 import org.jetbrains.annotations.NotNull;
 
 public class AutoMechanicTableJeiCategory extends AbstractRecipeCategory<AutoMechanicTableRecipe> {
-    /**
-     * @since 15.20.0
-     */
     public AutoMechanicTableJeiCategory() {
         super(
-                RecipeType.create(MoreMobility.MODID, "auto_mechanic_table", AutoMechanicTableRecipe.class),
+                MMJEIPlugin.AUTO_MECHANIC_TYPE,
                 Component.literal("aaa"),
                 new DrawableResource(SharedTexture.AUTO_MECHANIC_TEXTURE_ID, 0, 0, 16, 16, 0, 0, 0, 0, 16, 16),
-                9,
-                1
+                116,
+                54
         );
     }
 
@@ -30,5 +27,10 @@ public class AutoMechanicTableJeiCategory extends AbstractRecipeCategory<AutoMec
     public void setRecipe(@NotNull IRecipeLayoutBuilder builder, @NotNull AutoMechanicTableRecipe recipe, @NotNull IFocusGroup focuses) {
         recipe.getIngredients().forEach(ingredient -> builder.addInputSlot(0, 0).addIngredients(Ingredient.of(ingredient.getItems())));
         builder.addOutputSlot().addItemStack(recipe.getResultItem());
+    }
+
+    @Override
+    public void draw(@NotNull AutoMechanicTableRecipe recipe, @NotNull IRecipeSlotsView recipeSlotsView, @NotNull GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
     }
 }
--- src/main/java/com/sysnote8/moremobility/integration/jei/MMJEIPlugin.java
+++ src/main/java/com/sysnote8/moremobility/integration/jei/MMJEIPlugin.java
@@ -1,15 +1,21 @@
 package com.sysnote8.moremobility.integration.jei;
 
 import com.sysnote8.moremobility.MoreMobility;
+import io.github.foundationgames.automobility.recipe.AutoMechanicTableRecipe;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
 @JeiPlugin
 public class MMJEIPlugin implements IModPlugin {
     private static final ResourceLocation ID = MoreMobility.id("jei_plugin");
+    public static RecipeType<AutoMechanicTableRecipe> AUTO_MECHANIC_TYPE = RecipeType.create(MoreMobility.MODID, "auto_mechanic", AutoMechanicTableRecipe.class);
 
     @Override
     public @NotNull ResourceLocation getPluginUid() {
@@ -19,5 +25,12 @@ public class MMJEIPlugin implements IModPlugin {
     @Override
     public void registerCategories(@NotNull IRecipeCategoryRegistration registration) {
         registration.addRecipeCategories(new AutoMechanicTableJeiCategory());
+    }
+
+    @Override
+    public void registerRecipes(@NotNull IRecipeRegistration registration) {
+        ClientLevel world = Minecraft.getInstance().level;
+        if (world == null) return;
+        registration.addRecipes(AUTO_MECHANIC_TYPE, world.getRecipeManager().getAllRecipesFor(AutoMechanicTableRecipe.TYPE));
     }
 }
